e929e781fb205f36674bc4b9e2d93b2210117832
Clinton Davelaar <Davelaarprogramming@gmail.com>
2021-10-18 17:38:10 +0200

    Initial Commit

---
+++ CoffeeBrewAssessmentSwiftUi/CoffeeBrewAssessmentSwiftUiApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBrewAssessmentSwiftUiApp.swift
+//  CoffeeBrewAssessmentSwiftUi
+//
+//  Created by Clinton Davelaar on 18/10/2021.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBrewAssessmentSwiftUiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
