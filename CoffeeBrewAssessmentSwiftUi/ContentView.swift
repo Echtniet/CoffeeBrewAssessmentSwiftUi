02cde4c34093221cb3e8c09925f6775fe96a611d
Clinton Davelaar <Davelaarprogramming@gmail.com>
2021-10-18 19:16:28 +0200

    Retrieving JSON data

--- CoffeeBrewAssessmentSwiftUi/ContentView.swift
+++ CoffeeBrewAssessmentSwiftUi/ContentView.swift
@@ -6,11 +6,64 @@
 //
 
 import SwiftUI
+import Combine
+
+struct SubSelection: Decodable {
+    let _id: String
+    var name: String
+}
+
+struct Extra: Decodable {
+    let _id: String
+    var name: String
+    var subselections: [SubSelection]
+    
+}
+
+struct Size: Decodable {
+    let _id: String
+    var name: String
+}
+
+struct Coffee: Decodable {
+    let _id: String
+    var name: String
+    var sizes: [String]
+    var extras: [String]
+}
+
+struct CoffeeMachine: Decodable {
+    let _id: String
+    var types: [Coffee]
+    var sizes: [Size]
+    var extras: [Extra]
+}
+
+
 
 struct ContentView: View {
+    
+    @State private var coffeeMachine: CoffeeMachine?
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        HStack{
+            Text(coffeeMachine?.extras[1].name ?? "")
+        }.onAppear(perform: loadData)
+    }
+    
+    private func loadData() {
+        guard let url = URL(string: "https://darkroastedbeans.coffeeit.nl/coffee-machine/60ba1ab72e35f2d9c786c610") else {
+                return
+            }
+        URLSession.shared.dataTask(with: url) {data, response, error in
+            guard let data = data else { return }
+            if let decodedData = try? JSONDecoder().decode(CoffeeMachine.self, from: data){
+                DispatchQueue.main.async {
+                    self.coffeeMachine = decodedData
+                }
+            }
+        }.resume()
+        
     }
 }
 
@@ -19,3 +72,4 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
