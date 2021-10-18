52c1eadbddf67a6b4c624ee0af28751dd628cff8
Clinton Davelaar <DavelaarProgramming@gmail.com>
2021-10-18 21:37:38 +0200

    HomeView Complete with working link to coffee selection

--- CoffeeBrewAssessmentSwiftUi/ContentView.swift
+++ CoffeeBrewAssessmentSwiftUi/ContentView.swift
@@ -45,10 +45,57 @@ struct ContentView: View {
     
     @State private var coffeeMachine: CoffeeMachine?
     
+    
     var body: some View {
-        HStack{
-            Text(coffeeMachine?.extras[1].name ?? "")
-        }.onAppear(perform: loadData)
+        HomeView()
+//        HStack{
+//            Text(coffeeMachine?.extras[1].name ?? "")
+//        }.onAppear(perform: loadData)
+    }
+    
+//
+}
+
+struct HomeView: View {
+    var body: some View{
+        NavigationView {
+            VStack(alignment: .leading) {
+                Text("Tab the machine to start")
+                    .padding(.leading, 20)
+                    .padding(.top, -90)
+                    .font(.system(size: 30))
+                NavigationLink(destination: CoffeeView()){
+                    Image("home")
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                }
+                Text("How does this work?")
+                    .underline()
+                    .padding(.leading, 20)
+            }
+            .navigationBarTitle(Text("Dark Roasted Beans"))
+        }
+//        Image(trend.imageName)
+//        .resizable()
+//        .scaledToFit()
+    }
+}
+
+struct CoffeeView: View {
+    @State private var coffeeMachine: CoffeeMachine?
+    
+    
+    var body: some View {
+            VStack {
+                
+                VStack {
+                    
+                }
+            }
+            .onAppear(perform: loadData)
+            .navigationTitle("Brew with Lex")
+            .navigationBarBackButtonHidden(true)
+            Text("Please show coffee")
     }
     
     private func loadData() {
@@ -63,7 +110,7 @@ struct ContentView: View {
                 }
             }
         }.resume()
-        
+
     }
 }
 
